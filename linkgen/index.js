fb171dc69f2987e1fdacc0e87a535b5ab6f0ce94
HritikDutta <dutta.hritik@gmail.com>
2024-05-25 09:54:25 +0530

    Options are added based on options.txt

--- linkgen/index.js
+++ linkgen/index.js
@@ -1,15 +1,31 @@
 let linkText, linkLockButton;
-let generatedLinkLabel;
+let generatedLinkLabel, generateButton;
 let sourceSelector, campaignSelector;
 
 function onLoad() {
+  generateButton = document.getElementById('generate_button');
+  generateButton.readOnly = true;
+
   linkText = document.getElementById('link_text');
   linkLockButton = document.getElementById('link_text_lock');
   generatedLinkLabel = document.getElementById('generated_link');
 
   // Get Option References
   sourceSelector = document.getElementById('source');
+  sourceSelector.readOnly = true;
+
   campaignSelector = document.getElementById('campaign');
+  campaignSelector.readOnly = true;
+
+  { // Load and setup options
+    // const filepath = "https://hritikdutta.github.io/linkgen/options.txt"; // Only for testing offline!
+    
+    const filepath = document.URL.slice(0, -("index.html").length) + "options.txt";
+    loadFileAsText(filepath, contents => {
+      fillOptions(contents);
+      campaignSelector.readOnly = sourceSelector.readOnly = generateButton.readOnly = false;
+});
+  }
 }
 
 function generateLink() {
@@ -51,4 +67,63 @@ function appendUtmCode(generatedLink, isFirst, selector) {
 function toggleLinkLock() {
   linkText.readOnly = !linkText.readOnly;
   linkLockButton.innerText = linkText.readOnly ? 'Unlock' : 'Lock';
+}
+
+function loadFileAsText(filepath, callback) {
+  var request = new XMLHttpRequest();
+
+  request.onreadystatechange = function() {
+    if (request.readyState != 4 || request.status != 200) {
+      return;
+    }
+
+    // Remove all '\r' cause they just create confusion
+    let contents = request.responseText.replace('\r', '');
+    callback(contents);
+  }
+
+  request.open("GET", filepath, false);
+  request.send(null);
+}
+
+function fillOptions(contents) {
+  let lines = contents.split('\n');
+
+  let currentSelector = null;
+
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i].trim();
+
+    if (!line) {
+      continue;
+    }
+
+    // Check for utm headers
+    if (line[0] == '[') {
+      if (line[line.length - 1] != ']') {
+        console.error(`Error(${i + 1}): UTM Headers must be enclosed between '[' and ']'`);
+        continue;
+      }
+
+      const header = line.slice(1, -1).toLowerCase();
+      let selector = document.getElementById(header);
+
+      if (!selector) {
+        console.error(`Error(${i + 1}): UTM Header '${line}' not implemented!`);
+        continue;
+      }
+
+      currentSelector = selector;
+      continue;
+    }
+
+    if (!currentSelector) {
+      console.error(`Error(${i + 1}): UTM Header not specified before option '${line}'`);
+      continue;
+    }
+
+    currentSelector.innerHTML = currentSelector.innerHTML.concat(
+      `      <option value="${line}">${line}</option>`
+    );
+  }
 }
